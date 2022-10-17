aaced6cee24fc61bb0597939d9bf5ad939970f44
OlafGG <baezauriel01@gmail.com>
2022-10-17 14:16:09 -0500

    Server express initialited

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("My app");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+  return res.status(200).send('Hola mi server en express');
+});
+
+
+app.listen(port, () => {
+  console.log(`Mi port es: ${port}`);
+});
