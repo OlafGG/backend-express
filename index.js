9dc000c9e3c8aada498d16a788bce97d255980c9
OlafGG <baezauriel01@gmail.com>
2022-10-19 13:53:36 -0500

    add new routes

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) =>{
   return res.status(200).send('Hola mi server en express');
 });
 
+app.get('/home', (req, res)=>{
+  return res.status(200).send('Home')
+});
+
+app.get('/categories', (req, res)=>{
+  return res.status(200).send('Hola, soy una categoria')
+});
+
+app.get('/nueva-ruta', (req, res)=>{
+  return res.status(200).send('Hola, soy una nueva ruta')
+});
+
+app.get('/productos', (req, res)=>{
+  return res.status(200).json({
+    name: 'Producto 1',
+    price: '1000'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Mi port es: ${port}`);
