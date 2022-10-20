82bffc2036a0658504a4befe6a91fa7b521a712b
OlafGG <baezauriel01@gmail.com>
2022-10-19 21:27:31 -0500

    new query parameters

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const faker = require('faker');
 const app = express();
 const port = 3000;
 
@@ -18,19 +19,47 @@ app.get('/nueva-ruta', (req, res)=>{
   return res.status(200).send('Hola, soy una nueva ruta')
 });
 
-app.get('/products', (req, res)=>{
-  return res.status(200).json([
-    {
-      name: 'Producto 1',
-      price: '1000'
-    },
-    {
-      name: 'Producto 2',
-      price: '2000'
-    }
-  ]);
+app.get('/people', (req, res) => {
+  res.json([{
+      name: 'Arturo',
+      type: 'employee'
+  }, {
+      name: 'Jimena',
+      type: 'customer'
+  }]);
 });
 
+app.get('/users', (req, res)=>{
+  const { limit, offset } = req.query;
+  if (limit && offset) {
+    return res.status(200).json({
+      limit,
+      offset
+    });
+  }else {
+    return res.status(200).send('No hay nada mano')
+  }
+});
+
+app.get('/products', (req, res)=>{
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+
+  }
+  return res.status(200).json(products);
+});
+
+
+app.get('products/filter', (req, res)=>{
+  res.status(200).send('soy un filtro');
+});
 
 app.get('/products/:id', (req, res)=>{
   const { id } = req.params;
@@ -58,7 +87,9 @@ app.get('/categories/:categoryId', (req, res) => {
     name: 'Others',
     products: 2000
   })
-})
+});
+
+
 
 app.get('/categories/:categoryId/products/:productId', (req, res)=>{
   const { categoryId, productId } = req.params;
@@ -68,15 +99,7 @@ app.get('/categories/:categoryId/products/:productId', (req, res)=>{
   });
 });
 
-app.get('/people', (req, res) => {
-  res.json([{
-      name: 'Arturo',
-      type: 'employee'
-  }, {
-      name: 'Jimena',
-      type: 'customer'
-  }]);
-});
+
 
 app.get('/people/:id', (req, res) => {
   const { id } = req.params;
